633b1d42dcee8ca18a1b410e06aa68a44f5e911c
shashank <shashankgm10@gmail.com>
2024-01-12 17:23:00 +0530

    done some coding

---
+++ temp.py
@@ -0,0 +1,16 @@
+import numpy as np
+i=10
+j=12
+k=i*j
+k
+for i in range(1,10):
+    i<1
+    print('i is less than 5')
+    i =+1
+print('i is greater than 5')
+
+i=20
+if i<15:
+    print('i is lessthan 15')
+else:
+    print('i is greter than 15')
