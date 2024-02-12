d4f7dee6a21b0a27d03f533b05b99cf445a9b9c5
shashank <shashankgm10@gmail.com>
2024-02-12 19:57:45 +0530

    slicing and type castings are done

---
+++ DAY-2.py
@@ -0,0 +1,30 @@
+# type casting 
+
+x = 1 #int
+y = 2.0 #float
+z = "3" #str
+
+x=float(x)
+y=int(y)
+z=int(z)
+
+print(x)
+print(y)
+print(z)
+
+#Slicing 
+
+name="hello world "
+
+first_name=name[:4]
+last_name=name[5:]
+funky_name = name[::2]
+reveres_name=name[::-1]
+
+first_name
+last_name
+funky_name 
+reveres_name
+
+
+
