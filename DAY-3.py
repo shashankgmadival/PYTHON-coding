f942a324992d2a548b7dac284bc34b2abe368afb
shashank <shashankgm10@gmail.com>
2024-02-15 21:59:06 +0530

    list, tuple,set are done

---
+++ DAY-3.py
@@ -0,0 +1,38 @@
+# list
+
+food=['dosa','idli','sambar','ice-cream']
+
+food[0]='sushi'
+
+food
+
+food.append('dosa') # it will add the item in last position
+food
+
+food.remove('dosa')
+food
+
+food.pop() # it will remove the last variable
+food
+
+food.insert(0,'pizza') #it will insert pizza in index 0
+food
+
+food.sort()
+food
+
+food
+food.insert(2,'ice-cream')
+food
+
+# insert 10 in between 3 and 4 
+list=[1,2,3,4,5,6]
+list.insert(3,10)
+list
+
+for i in list:
+    print(i)
+
+
+# 2D-LIST
+    
