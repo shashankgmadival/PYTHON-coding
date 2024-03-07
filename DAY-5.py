9536b9746f0b2605424fc7f7e1090375ad4becf3
shashank <shashankgm10@gmail.com>
2024-03-07 20:47:53 +0530

    code

---
+++ DAY-5.py
@@ -0,0 +1,9 @@
+print('i am good boy')
+
+# use \ this for continueing the line
+print('shasshnsjwdnasd\
+fhdifnefeiwfeifoe')
+
+jfvfurgrkufjfgifgjogkg # use forward slash for commenting and uncommenting the line
+
+print('sha',55,8,sep='-',end='08\n')
