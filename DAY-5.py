024becbdb97804f22d2d7182074303df0b44c2ed
shashank <shashankgm10@gmail.com>
2024-03-07 21:18:06 +0530

    coding

--- DAY-5.py
+++ DAY-5.py
@@ -7,3 +7,15 @@ fhdifnefeiwfeifoe')
 jfvfurgrkufjfgifgjogkg # use forward slash for commenting and uncommenting the line
 
 print('sha',55,8,sep='-',end='08\n')
+
+list=[1,20.20,'shashank',['shasha',10],20020]
+list[4]
+list[3]
+list[3][0]
+
+tuple=([list,1,20.20,['shashank',['shasha',10],20020],1991919])
+# print(tuple)
+
+tuple[3][1][1]
+tuple[4]
+tuple[3][2]
