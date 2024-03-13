726c1170d9a58784a3aa824e06402e7455dcb863
shashank <shashankgm10@gmail.com>
2024-03-13 20:52:48 +0530

    loops

---
+++ DAY-8.py
@@ -0,0 +1,20 @@
+import random
+
+i=[random.randint(1,10) in range(10)]
+print(i)
+
+
+name='shashank'
+
+for i in name:
+    if i=='s':
+        print('g')
+    else:
+        print(i)
+
+
+colors=['red','green ']
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
