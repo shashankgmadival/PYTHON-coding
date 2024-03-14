9636c8a1127452ee36033eaad7cfadb565019b68
shashank <shashankgm10@gmail.com>
2024-03-14 18:29:27 +0530

    continue and break

---
+++ DAY-9.py
@@ -0,0 +1,14 @@
+for i in range(12):
+    if(i==10):
+        break
+    print('6 X ',i , '=',6*i)
+
+
+
+for i in range(1,101,1):
+    print(i)
+    if(i==50):
+        break
+    else:
+        print('dfhdbff')
+print('thankyou')
