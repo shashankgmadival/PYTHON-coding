4627dfd7f9d4331996fed7c5d3ba73eaf74a13f5
shashank <shashankgm10@gmail.com>
2024-03-14 18:35:17 +0530

    end and sep

--- DAY-9.py
+++ DAY-9.py
@@ -12,3 +12,28 @@ for i in range(1,101,1):
     else:
         print('dfhdbff')
 print('thankyou')
+
+
+print('hello', end='')
+print('world')
+
+print("Hello", end="")
+print("World")
+# Output: HelloWorld
+
+print("Hello", end="***")
+print("World")
+# Output: Hello***World
+
+
+print("apple", "banana", "cherry")
+# Output: apple banana cherry
+
+print("apple", "banana", "cherry", sep=", ")
+# Output: apple, banana, cherry
+
+print("one", "two", "three", sep="-")
+# Output: one-two-three
+
+print(1, 2, 3, 4, 5, sep=" | ")
+# Output: 1 | 2 | 3 | 4 | 5
