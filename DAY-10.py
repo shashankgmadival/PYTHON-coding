963351ccd239eea896bcb9a20cf478019acc35ff
shashank <shashankgm10@gmail.com>
2024-03-20 20:17:34 +0530

    functions

---
+++ DAY-10.py
@@ -0,0 +1,17 @@
+def addition(a=10,b=20):
+    print('the addition of the number is',a+b)
+
+
+addition(15,20)
+
+
+
+def averag(10,10):
+    print('averagde',(a+b)/2)
+
+averag(10,10)
+
+averag(7,5)
+
+
+
