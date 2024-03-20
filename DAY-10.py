2a226b5aaa32c85de7933c0227eabbb777c8feee
shashank <shashankgm10@gmail.com>
2024-03-20 20:36:29 +0530

    checked some list and tuple

--- DAY-10.py
+++ DAY-10.py
@@ -13,5 +13,30 @@ averag(10,10)
 
 averag(7,5)
 
+l=[1,10,11,20,25]
+l[2]
+print(l.index(2))
+
+print(l)
+l.reverse()
+print(l)
+
+print(l.index(10))
+l.insert(2,1000)
+print(l)
+
+print(l)
+l[2]=111
+print(l)
 
 
+l=[10,20,30,40]
+l[2]=50
+l
+
+l.insert(2,60)
+l
+
+
+tup=(10,20,10)
+tup[2]
